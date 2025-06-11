435e98f88b7a292eb8325646245b0d1bc2801f09
Kshitiz Bisht <60011595+KshitizBisht@users.noreply.github.com>
2025-06-11 11:15:19 +0100

    initial backend

---
+++ backend/battery-dashboard/src/main/java/com/batterydashboard/batterydashboard/BatteryDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.batterydashboard.batterydashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatteryDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatteryDashboardApplication.class, args);
+    }
+
+}
---
+++ backend/battery-dashboard/src/test/java/com/batterydashboard/batterydashboard/BatteryDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.batterydashboard.batterydashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatteryDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
