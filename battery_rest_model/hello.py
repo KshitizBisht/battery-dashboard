30418bc4b0a3cc79007c8cbe7109774fd26745f9
“KshitizBisht” <kshtzbsht@gmail.com>
2025-06-11 20:03:20 +0100

    flask changes

--- battery_rest_model/hello.py
+++ battery_rest_model/hello.py
@@ -1,16 +1,18 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
    return 'Hello World'
 
-@app.route('/predict-soh', methods=['GET'])
+@app.route('/predict-soh', methods=['POST'])
 def predict_soh():
+   print(request.get_json())
    return jsonify({"soh":90})
 
-@app.route('/predict-soc', methods=['GET'])
-def predict_soh():
+@app.route('/predict-soc', methods=['POST'])
+def predict_soc():
+   print(request.get_json())
    return jsonify({"soc":40})
 
 if __name__ == '__main__':
