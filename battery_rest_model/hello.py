a4606b7d814cd2dfd060cd177b2fad309ad4c3a7
“KshitizBisht” <kshtzbsht@gmail.com>
2025-06-11 14:57:36 +0100

    add placeholder endpoint for soh

--- battery_rest_model/hello.py
+++ battery_rest_model/hello.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
    return 'Hello World'
+
+@app.route('/predict-soh', methods=['GET'])
+def predict_soh():
+   return jsonify({"soh":90})
 
 if __name__ == '__main__':
    app.run()
