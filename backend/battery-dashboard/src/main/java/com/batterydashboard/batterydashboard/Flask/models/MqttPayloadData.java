9f8d2035e1315b9295b5c573824d7d5a00b29ae6
Josh Webster <joshuadanielwebstyer@gmail.com>
2025-06-12 11:04:35 +0100

    commit

--- backend/battery-dashboard/src/main/java/com/batterydashboard/batterydashboard/Flask/models/MqttPayloadData.java
+++ backend/battery-dashboard/src/main/java/com/batterydashboard/batterydashboard/Flask/models/MqttPayloadData.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import java.time.Instant;
 
 @Data
+
 public class MqttPayloadData {
 
     private String batteryId;
@@ -15,4 +16,11 @@ public class MqttPayloadData {
     private float loadCurrent;
     private float loadVoltage;
     private float time;
+    private String soc; // changed from float to String
+    private Capacity capacity;
+
+    public static class Capacity {
+        private float nominal;
+        private float estimatedRemaining;
+    }
 }
